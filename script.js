0be138c1012d8bf1c92f8fbafc69de4621d19b20
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-27 16:17:36 +0300

    added a Second class that calls the hello method from the First class

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+class First {
+  hello() {
+    console.log("Привет, я метод родителя!");
+  }
+}
+
+class Second extends First {
+  hello() {
+    const resultFirst = new First();
+    resultFirst.hello();
+    console.log("А я - наследуемый объект!");
+  }
+}
+
+const result = new Second();
+result.hello();
